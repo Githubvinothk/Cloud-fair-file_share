99b6f99c68d9a53add67fc347eba84759a28b427
Vinothkumar T <120093514+Githubvinothk@users.noreply.github.com>
2024-03-31 16:35:32 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -35,7 +35,7 @@ async def channel_post(client: Client, message: Message):
         caption = post_message.caption if post_message.caption else ""
         file_size = humanbytes(media.file_size) if media.file_size else ""
         
-        text = f"""<b>{post_message.caption}</b>\n\n<b>🔮 Size: {file_size}\n\n{link}</b>"""
+        text = f"""<b>{post_message.caption}</b>\n\n<b>🔮 Size: {file_size}\n\n🔗 {link}</b>"""
     else:
         # No media, send only the link
         text = f"<b>Here is your link: {link}</b>"
